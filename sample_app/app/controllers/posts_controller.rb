73da98e817019d9ee47f87193c7232955020eb8d
Naoki Yano <vegetables412@gmail.com>
2018-05-14 02:32:36 +0900

    Finish 3-7

---
+++ sample_app/app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  end
+
+	def create
+        # ストロングパラメーターを使用
+         post = Post.new(post_params)
+        # DBへ保存する
+         post.save
+         redirect_to post_path(post.id) # 詳細画面へリダイレクト
+    end
+
+    def index
+    	@posts = Post.all
+    end
+
+    def show
+    	@post = Post.find(params[:id])
+    end
+
+	def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+    	post = Post.find(params[:id])
+        post.update(post_params)
+        redirect_to post_path(post.id)
+    end
+
+  private
+
+	def post_params
+    	params.require(:post).permit(:title, :body)
+	end
+end

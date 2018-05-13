9603c0ceb6358b9986c70e2e3f8de231066ebc46
Naoki Yano <vegetables412@gmail.com>
2018-05-14 03:59:40 +0900

    Finish 3-8

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513140738) do
+ActiveRecord::Schema.define(version: 20180513180653) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
